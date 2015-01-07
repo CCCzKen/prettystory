efa3d35ad47d57a569a509f8de0c230520deb041
15919338500 <15919338500@1a0beb16-39a9-491a-8432-0dfbb1476d54>
2015-01-07 23:38:43 +0000

    123

--- index.py
+++ index.py
@@ -1,11 +1,6 @@
 #-*- coding:utf-8 -*-
-import flask
-def app(environ, start_response):
-	status = '200 OK'
-	headers = [('Content-type', 'text/html')]
-	start_response(status, headers)
-	body=["Welcome to Baidu Cloud!\n"]
-	return body
+from src import web_app
+
 
 from bae.core.wsgi import WSGIApplication
-application = WSGIApplication(app)
+application = WSGIApplication(web_app)
