a82f1da1994a43f93a50317f09ef5d39dd9ffc2b
15919338500 <15919338500@1a0beb16-39a9-491a-8432-0dfbb1476d54>
2015-01-07 23:33:53 +0000

    123

--- index.py
+++ index.py
@@ -4,7 +4,8 @@ def app(environ, start_response):
 	status = '200 OK'
 	headers = [('Content-type', 'text/html')]
 	start_response(status, headers)
-	return 'Hello world'
+	body=["Welcome to Baidu Cloud!\n"]
+	return body
 
 from bae.core.wsgi import WSGIApplication
 application = WSGIApplication(app)
