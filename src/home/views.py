0aa988cec3d3fadd65d29bd4c95b565761b7b512
15919338500 <15919338500@1a0beb16-39a9-491a-8432-0dfbb1476d54>
2015-01-08 01:18:05 +0000

    123

--- src/home/views.py
+++ src/home/views.py
@@ -1,6 +1,8 @@
 # coding: utf-8
+import hashlib
+import time
 from flask import render_template, request, Blueprint
-from flask import url_for, redirect
+from flask import url_for, redirect, make_response
 
 bp = Blueprint('home', __name__)
 
@@ -8,3 +10,19 @@ bp = Blueprint('home', __name__)
 @bp.route('/')
 def home():
 	return 'Hello World'
+
+@bp.route('wechat', methods=['GET', 'POST'])
+def wechat_auth():
+	if request.method == 'GET':
+		token = 'prettystory'
+		data = request.args
+		signature = data.signature
+		timestamp = data.timestamp
+		nonce = data.nonce
+		echostr = data.echostr
+		args = [signature, nonce, token]
+		args.sort()
+		args = ''.join(args)
+		hashcode = hashlib.sha1(args).hexdigest() 
+		if hashcode == signature :
+			return make_response(echostr)
