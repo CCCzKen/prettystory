ffaadb24be1dc0c762f8fb03ff3d90c74eff2c6f
15919338500 <15919338500@1a0beb16-39a9-491a-8432-0dfbb1476d54>
2015-01-08 08:18:13 +0000

    123

--- src/home/views.py
+++ src/home/views.py
@@ -24,29 +24,28 @@ def home():
 
 @bp.route('wechat/', methods=['GET', 'POST'])
 def wechat_auth():
-	token = 'prettystory'
-	query = request.args
-	signature = query.get('signature', '')
-	timestamp = query.get('timestamp', '')
-	nonce = query.get('nonce', '')
-	echostr = query.get('echostr', '')
-	data = [token, timestamp, nonce]
-	data.sort()
-	data = ''.join(data)
-	hashcode = hashlib.sha1(data).hexdigest()
-	if hashcode == signature:
-		return make_response(echostr)
-	return 'false'
-
-@bp.route('reply/', methods=['POST'])
-def reply():
-	xml = etree.fromstring(request.data)
-	content = xml.find('Content').text
-	fromUser = xml.find('FromUserName').text
-	toUser = xml.find('ToUserName').text
-	msg = u'我现在还在开发中，还没有什么功能，您刚才说的是：' + content
-	response = make_response(xml_text % (toUser, FromUserName, str(int(time.time())), msg))
-	return response
+	if request.method == 'GET':
+		token = 'prettystory'
+		query = request.args
+		signature = query.get('signature', '')
+		timestamp = query.get('timestamp', '')
+		nonce = query.get('nonce', '')
+		echostr = query.get('echostr', '')
+		data = [token, timestamp, nonce]
+		data.sort()
+		data = ''.join(data)
+		hashcode = hashlib.sha1(data).hexdigest()
+		if hashcode == signature:
+			return make_response(echostr)
+		return 'false'
+	else:
+		xml = etree.fromstring(request.data)
+		content = xml.find('Content').text
+		fromUser = xml.find('FromUserName').text
+		toUser = xml.find('ToUserName').text
+		msg = u'我现在还在开发中，还没有什么功能，您刚才说的是：' + content
+		response = make_response(xml_text % (toUser, FromUserName, str(int(time.time())), msg))
+		return response
 
 
 # @bp.route('reply/')
