979b72b4b0dcb0ea0451cee35fc978e56d90372e
15919338500 <15919338500@1a0beb16-39a9-491a-8432-0dfbb1476d54>
2015-01-08 22:25:18 +0000

    123

--- src/home/views.py
+++ src/home/views.py
@@ -24,7 +24,7 @@ def wechat_auto():
 		fromUser = xml.find('FromUserName').text
 		toUser = xml.find('ToUserName').text
 		msgId = xml.find('MsgId').text
-		msg = u'我现在还在开发中，还没有什么功能，您刚才说的是：' + content
+		msg = u'我现在还在开发中，还没有什么功能，您刚才说的是：' + content.encode('utf-8')
 		response = make_response(xml_text % (toUser, fromUser, str(int(time.time())), msg, msgId))
 		response.content_type='application/xml'
 		return response
