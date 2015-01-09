13088261f125c285c71a836782c3aa002112a1ca
15919338500 <15919338500@1a0beb16-39a9-491a-8432-0dfbb1476d54>
2015-01-09 07:14:55 +0000

    123

--- src/settings.py
+++ src/settings.py
@@ -6,6 +6,14 @@ TESTING = True
 
 TOKEN = 'prettystory'
 
+MSG_TEXT_TPL = """<xml>
+<ToUserName><![CDATA[%s]]></ToUserName>
+<FromUserName><![CDATA[%s]]></FromUserName>
+<CreateTime>%s</CreateTime>
+<MsgType><![CDATA[text]]></MsgType>
+<Content><![CDATA[%s]]></Content>
+</xml>"""
+
 ROOT_FOLDER = os.path.dirname(os.path.abspath(__file__))
 TEMPLATE_FOLDER = ROOT_FOLDER + '/templates/'
 STATIC_FOLDER = ROOT_FOLDER + '/static/'
