ff5cdbec90707f5de588b7bf6827db6e19e9e1eb
15919338500 <15919338500@1a0beb16-39a9-491a-8432-0dfbb1476d54>
2015-01-09 11:05:47 +0000

    123

---
+++ src/function/lyrics.py
@@ -0,0 +1,38 @@
+# coding: utf-8
+import re
+from requests import get
+
+human_headers = {
+	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+	'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:31.0) Gecko/20100101 Firefox/31.0',
+	'Accept-Encoding':'gzip,deflate,sdch'
+}
+
+
+class Lyrics(object):
+	def __init__(self, song, singer=None):
+		self.song = song
+		self.singer = singer
+
+	def find(self):
+		url = 'http://music.baidu.com/search/lrc?from=new_mp3&key=%s+%s' % (self.song, self.singer)
+		html = get(url, headers=human_headers, ).text
+		return html
+
+	def extract(self):
+		page = self.find()
+		text = re.search(r'<p id="lyricCont-0">([\S\s]*?)</p>', page).group(1)
+		return text
+
+	def filter(self):
+		text = self.extract()
+		text = re.sub(r'[ <em>br/]', '', text)
+		print text
+
+
+def main():
+	do = Lyrics('心跳', '王力宏')
+	do.filter()
+
+if __name__ == '__main__':
+	main()
