031fb121f687bc1437427bbcbf615605c8da09ba
15919338500 <15919338500@1a0beb16-39a9-491a-8432-0dfbb1476d54>
2015-01-09 06:12:04 +0000

    123

--- src/wechat/views.py
+++ src/wechat/views.py
@@ -1,11 +1,29 @@
 # coding: utf-8
+import hashlib
+from src import settings
 from flask import render_template, Blueprint, request
 from flask import make_response, redirect, url_for
 
 bp = Blueprint('wechat', __name__)
 
 
-@bp.route('/')
-def wechat_auth():
-	query = request.args
-	return 'pretty'
+@bp.route('/', methods=['GET'])
+def wechat_access_verify():
+	echostr = request.args.get('echostr', '')
+	if verification(request):
+		return echostr
+	return 'something wrong'
+
+
+def verification(request):
+	token = settings.TOKEN
+	signature = request.args.get('signature', '')
+	timestamp = request.args.get('timestamp', '')
+	nonce = request.args.get('nonce', '')
+	data = [token, timestamp, nonce]
+	data.sort()
+	data = ''.join(data)
+	hashcode = hashlib.sha1(data).hexdigest()
+	if hashcode == signature:
+		return True
+	return False
