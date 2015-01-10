a15e8fabe3c4b234864928156344373cc4595b0b
15919338500 <15919338500@1a0beb16-39a9-491a-8432-0dfbb1476d54>
2015-01-10 08:30:13 +0000

    123

--- src/settings.py
+++ src/settings.py
@@ -21,16 +21,14 @@ MSG_EVENT_TPL = """<xml>
 <CreateTime>%s</CreateTime>
 <MsgType><![CDATA[event]]></MsgType>
 <Event><![CDATA[subscribe]]></Event>
-</xml>
-"""
+</xml>"""
 
 ERROR_TEXT = u'公众号处于研发阶段，不能自动回复您的消息，请等待开发者回复。'
 ERROR_SONG = u'很抱歉，没有这首歌曲'
 
 HELP_INFO = u'''公众号处于研发阶段，目前实现的功能有：
 查歌词——歌词：只是太爱你 张敬轩
-若有什么新的想法，可以直接在此留言。
-'''
+若有什么新的想法，可以直接在此留言。'''
 
 RULE = r'歌词:'
 
